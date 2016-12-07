01bedc1068ca9a6386403f554d387ebb556b183c
rashemihmih <mihmih96@gmail.com>
2016-12-07 18:18:52 +0300

    improved GameMechTest

--- src/test/java/GameMechTest.java
+++ src/test/java/GameMechTest.java
@@ -1,32 +1,95 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.mail.park.Application;
-import ru.mail.park.game.mechanics.GameSession;
-import ru.mail.park.game.mechanics.Player;
+import ru.mail.park.game.GameMechService;
 import ru.mail.park.game.mechanics.Square;
 import ru.mail.park.game.messaging.PlayerAction;
+import ru.mail.park.game.messaging.ServerSnap;
 import ru.mail.park.model.UserProfile;
+import ru.mail.park.model.exception.UserAlreadyExistsException;
+import ru.mail.park.services.AccountService;
+import ru.mail.park.websocket.Message;
+import ru.mail.park.websocket.RemotePointService;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
-@SuppressWarnings("SpringJavaAutowiredMembersInspection")
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings({"SpringJavaAutowiredMembersInspection", "ConstantConditions"})
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class GameMechTest {
+    @MockBean
+    private RemotePointService remotePointService;
+    @Autowired
+    private GameMechService gameMechService;
+    @Autowired
+    private AccountService accountService;
+
+    private UserProfile first;
+    private UserProfile second;
+    private Queue<Message> messages = new ArrayDeque<>();
+
+    @Before
+    public void init() throws IOException {
+        when(remotePointService.isConnected(any())).thenReturn(true);
+        doAnswer(invocationOnMock -> {
+            final Object[] args = invocationOnMock.getArguments();
+            messages.add((Message) args[1]);
+            return null;
+        }).when(remotePointService).sendMessageToUser(any(), any());
+        try {
+            accountService.addUser("TestUser-1", "TestPassword-1", "TestEmail-1");
+        } catch (UserAlreadyExistsException ignore) {
+        }
+        try {
+            accountService.addUser("TestUser-2", "TestPassword-2", "TestEmail-2");
+        } catch (UserAlreadyExistsException ignore) {
+        }
+        first = accountService.getUserByLogin("TestUser-1");
+        second = accountService.getUserByLogin("TestUser-2");
+    }
+
     @Test
-    public void testAction() {
-        final Player first = new Player(new UserProfile("a", "b", "c"));
-        final Player second = new Player(new UserProfile("q", "w", "e"));
-        final GameSession session = new GameSession(first, second);
-        final PlayerAction action = new PlayerAction();
-        action.setRow(1);
-        action.setCol(2);
-        action.setPositive(true);
-        session.processAction(first, action);
+    public void test() throws IOException {
+        gameMechService.addPlayer(first);
+        gameMechService.addPlayer(second);
+        final int[][] initialMatrix = new Square().getMatrix();
+        testServerSnap(first, second, messages.poll(), initialMatrix, initialMatrix);
+        testServerSnap(second, first, messages.poll(), initialMatrix, initialMatrix);
+        final PlayerAction playerAction = new PlayerAction();
+        playerAction.setRow(1);
+        playerAction.setCol(2);
+        playerAction.setPositive(true);
+        gameMechService.addPlayerAction(first, playerAction);
         final Square square = new Square();
         square.activate(1, 2, 2, 1);
-        assertEquals(square, session.getFirst().getSquare());
+        testServerSnap(first, second, messages.poll(), square.getMatrix(), initialMatrix);
+        testServerSnap(second, first, messages.poll(), initialMatrix, square.getMatrix());
+    }
+
+    private void testServerSnap(UserProfile player, UserProfile opponent, Message message, int[][] playerMatrix,
+                                int[][] opponentMatrix) {
+        assertEquals(ServerSnap.class.getSimpleName(), message.getType());
+        final JSONObject content = new JSONObject(message.getContent());
+        assertEquals(player.getLogin(), content.getString("player"));
+        assertEquals(opponent.getLogin(), content.getString("opponent"));
+        assertEquals(new JSONArray(playerMatrix).toString(), content.getJSONArray("playerMatrix").toString());
+        assertEquals(new JSONArray(opponentMatrix).toString(), content.getJSONArray("opponentMatrix").toString());
+        assertNotEquals(new JSONArray(playerMatrix).toString(), content.getJSONArray("target").toString());
+        assertFalse(content.getBoolean("gameOver"));
+        assertFalse(content.getBoolean("win"));
     }
 }
