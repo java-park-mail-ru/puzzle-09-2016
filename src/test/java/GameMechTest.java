a55db182ad178836c65b6fcf72ac3762b0f0bb44
rashemihmih <mihmih96@gmail.com>
2016-12-08 00:15:06 +0300

    improved GameMechTest

--- src/test/java/GameMechTest.java
+++ src/test/java/GameMechTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.mail.park.Application;
 import ru.mail.park.game.GameMechService;
+import ru.mail.park.game.mechanics.GameSession;
 import ru.mail.park.game.mechanics.Square;
 import ru.mail.park.game.messaging.PlayerAction;
 import ru.mail.park.game.messaging.ServerSnap;
@@ -19,10 +20,12 @@ import ru.mail.park.websocket.Message;
 import ru.mail.park.websocket.RemotePointService;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayDeque;
+import java.util.Map;
 import java.util.Queue;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
@@ -62,13 +65,25 @@ public class GameMechTest {
         second = accountService.getUserByLogin("TestUser-2");
     }
 
+    @SuppressWarnings({"OverlyBroadThrowsClause", "unchecked"})
     @Test
-    public void test() throws IOException {
+    public void test() throws Exception {
         gameMechService.addPlayer(first);
         gameMechService.addPlayer(second);
+        final Field sessionsField = gameMechService.getClass().getDeclaredField("sessions");
+        sessionsField.setAccessible(true);
+        final Map<UserProfile, GameSession> sessions = (Map<UserProfile, GameSession>) sessionsField.get(gameMechService);
+        final GameSession session = sessions.get(first);
+        final Field targetField = session.getClass().getDeclaredField("target");
+        targetField.setAccessible(true);
         final int[][] initialMatrix = new Square().getMatrix();
-        testServerSnap(first, second, messages.poll(), initialMatrix, initialMatrix);
-        testServerSnap(second, first, messages.poll(), initialMatrix, initialMatrix);
+        final int[][] target = ((Square) targetField.get(session)).getMatrix();
+        testServerSnap(first, second, messages.poll(), initialMatrix, initialMatrix, target, false, false);
+        testServerSnap(second, first, messages.poll(), initialMatrix, initialMatrix, target, false, false);
+        final Square newTarget = new Square();
+        newTarget.activate(1, 2, 2, 1);
+        newTarget.activate(3, 4, 2, 1);
+        targetField.set(session, newTarget);
         final PlayerAction playerAction = new PlayerAction();
         playerAction.setRow(1);
         playerAction.setCol(2);
@@ -76,20 +91,31 @@ public class GameMechTest {
         gameMechService.addPlayerAction(first, playerAction);
         final Square square = new Square();
         square.activate(1, 2, 2, 1);
-        testServerSnap(first, second, messages.poll(), square.getMatrix(), initialMatrix);
-        testServerSnap(second, first, messages.poll(), initialMatrix, square.getMatrix());
+        testServerSnap(first, second, messages.poll(), square.getMatrix(), initialMatrix, newTarget.getMatrix(),
+                false, false);
+        testServerSnap(second, first, messages.poll(), initialMatrix, square.getMatrix(), newTarget.getMatrix(),
+                false, false);
+        final PlayerAction winningAction = new PlayerAction();
+        winningAction.setRow(3);
+        winningAction.setCol(4);
+        winningAction.setPositive(true);
+        gameMechService.addPlayerAction(first, winningAction);
+        testServerSnap(first, second, messages.poll(), newTarget.getMatrix(), initialMatrix, newTarget.getMatrix(),
+                true, true);
+        testServerSnap(second, first, messages.poll(), initialMatrix, newTarget.getMatrix(), newTarget.getMatrix(),
+                true, false);
     }
 
     private void testServerSnap(UserProfile player, UserProfile opponent, Message message, int[][] playerMatrix,
-                                int[][] opponentMatrix) {
+                                int[][] opponentMatrix, int[][] target, boolean gameOver, boolean win) {
         assertEquals(ServerSnap.class.getSimpleName(), message.getType());
         final JSONObject content = new JSONObject(message.getContent());
         assertEquals(player.getLogin(), content.getString("player"));
         assertEquals(opponent.getLogin(), content.getString("opponent"));
         assertEquals(new JSONArray(playerMatrix).toString(), content.getJSONArray("playerMatrix").toString());
         assertEquals(new JSONArray(opponentMatrix).toString(), content.getJSONArray("opponentMatrix").toString());
-        assertNotEquals(new JSONArray(playerMatrix).toString(), content.getJSONArray("target").toString());
-        assertFalse(content.getBoolean("gameOver"));
-        assertFalse(content.getBoolean("win"));
+        assertEquals(new JSONArray(target).toString(), content.getJSONArray("target").toString());
+        assertEquals(gameOver, content.getBoolean("gameOver"));
+        assertEquals(win, content.getBoolean("win"));
     }
 }
