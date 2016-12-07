03371fa39b7431f4cf1e826a473ad5d54362f14f
rashemihmih <mihmih96@gmail.com>
2016-12-07 16:34:36 +0300

    optimized imports

--- src/main/java/ru/mail/park/game/GameMechService.java
+++ src/main/java/ru/mail/park/game/GameMechService.java
@@ -15,7 +15,10 @@ import ru.mail.park.services.AccountService;
 import ru.mail.park.websocket.RemotePointService;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
